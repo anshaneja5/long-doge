0b211718113fa2a58c1e10d0fe8ce06182f46a44
Ansh Aneja <128882734+anshaneja5@users.noreply.github.com>
2023-08-15 18:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const neck = document.querySelector(".neck");
-console.log("hi");
-const wrapper= document.querySelector(".wrapper");
-const doge=document.querySelector(".long-doge");
-const text=document.querySelector(".dy-text");
-let i=0;
+const wrapper = document.querySelector(".wrapper");
+const doge = document.querySelector(".long-doge");
+const text = document.querySelector(".dy-text");
+let i = 0;
+
 function appendText() {
     const newPre = document.createElement('pre');
     newPre.textContent = neck.innerText;
     i++;
-    text.innerText=`Your doge is ${i} times woww`;
+    text.innerText = `Your doge is ${i} times woww`;
     // Append the new paragraph to the scroll content
     doge.appendChild(newPre);
 }
 
-window.addEventListener('scroll', () => {
-    // Define the condition for appending text (e.g., scroll position)
-    if (window.scrollY > 30) { // Adjust the scroll threshold as needed
-        appendText();
-        appendwow();
-    }
-});
-
-function appendwow(){
+function appendWow() {
     let wow = document.createElement('div');
-    wow.innerText="WOW";
-    wow.className="wawo";
-    wow.style.left=Math.random()*window.innerWidth - 100 + 'px';
-    wow.style.top=wrapper.offsetHeight - 100 +'px';
+    wow.innerText = "WOW";
+    wow.className = "wawo";
+    wow.style.left = Math.random() * window.innerWidth - 100 + 'px';
+    wow.style.top = wrapper.offsetHeight - 100 + 'px';
     document.body.appendChild(wow);
 }
 
+function handleScroll() {
+    if (window.scrollY > 30) {
+        appendText();
+        appendWow();
+    }
+}
+
+// Use 'scroll' event for laptops/desktops
+window.addEventListener('scroll', handleScroll);
+
+// Use 'touchmove' event for mobile devices
+window.addEventListener('touchmove', handleScroll);
+
+// Use 'resize' event to update positioning when the window size changes
+window.addEventListener('resize', appendWow);
