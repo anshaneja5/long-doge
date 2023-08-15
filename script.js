68a32cd0b6a7f01cca777c89d7ee4079cf8c2908
Ansh Aneja <128882734+anshaneja5@users.noreply.github.com>
2023-08-15 18:05:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const neck = document.querySelector(".neck");
+console.log("hi");
+const wrapper= document.querySelector(".wrapper");
+const doge=document.querySelector(".long-doge");
+const text=document.querySelector(".dy-text");
+let i=0;
+function appendText() {
+    const newPre = document.createElement('pre');
+    newPre.textContent = neck.innerText;
+    i++;
+    text.innerText=`Your doge is ${i} times woww`;
+    // Append the new paragraph to the scroll content
+    doge.appendChild(newPre);
+}
+
+window.addEventListener('scroll', () => {
+    // Define the condition for appending text (e.g., scroll position)
+    if (window.scrollY > 30) { // Adjust the scroll threshold as needed
+        appendText();
+        appendwow();
+    }
+});
+
+function appendwow(){
+    let wow = document.createElement('div');
+    wow.innerText="WOW";
+    wow.className="wawo";
+    wow.style.left=Math.random()*window.innerWidth - 100 + 'px';
+    wow.style.top=wrapper.offsetHeight - 100 +'px';
+    document.body.appendChild(wow);
+}
+
